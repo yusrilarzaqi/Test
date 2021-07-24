d0806dd81c732ba185bee332ec3060a425b906a0
yusrilarzaqi <arzaqiyusril@gmail.com>
2021-07-25 00:05:40 +0700

    modified modified:   main.py

--- fileKu/main.py
+++ fileKu/main.py
@@ -1,6 +1,8 @@
 n = int(input(">> "))
 
-for i in range(n):
+for i in range(0, n):
     for j in range(i):
-        print("*", end="")
+        print(" ", end="")
+    for k in range(n, i, -1):
+        print("x", end="")
     print()
