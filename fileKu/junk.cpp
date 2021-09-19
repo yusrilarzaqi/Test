2f914581850bd04905943aa8cf34b79c106ae7ef
yusrilarzaqi <arzaqiyusril@gmail.com>
2021-09-19 22:49:29 +0700

    add new file

---
+++ fileKu/junk.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float pj, pr, lb, tg;
+    cout << ">> ";cin >> pj;
+    cout << ">> ";cin >> lb;
+    cout << ">> ";cin >> tg;
+    pr = 4 * pj+ lb +tg;
+    cout << pr << endl;
+    return 0;
+}
