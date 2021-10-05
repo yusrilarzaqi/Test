37c0dc9ce324329532860de6002459156ec5aba8
yusrilarazqi <arzaqiyusril@gmail.com>
2021-10-05 21:33:50 +0700

    modified fileKu/tugas/a.py

--- fileKu/tugas/a.py
+++ fileKu/tugas/a.py
@@ -1,10 +1,12 @@
 umur = int(input("masukan umur anda : "))
 
 if umur >= 18:
-    status_pendaftaran = int(input("masukan NIM anda : "))
-    if status_pendaftaran == 185512:
+    status = input("apakah anda terpilih [y/n]")
+    if status == 'y' or status == 'Y' :
         print("Anda sudah berhak memilih")
-    else:
+    elif status == 'n' or status == 'N':
         print("Status anda belum terdaftar")
+    else:
+        print("anda salah menginputkan")
 else:
     print("anda belu. berhak memilih")
